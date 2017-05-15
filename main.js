41246a950ebdcf22a5a83cbe690a07ce040ca7d0
Abby Starnes <abbymstarnes@gmail.com>
2017-05-15 13:34:53 -0500

    collision test working

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
    let robotBottom = '';
    let robotTop = '';
    let setup = false;
+   let lives = 3;
    // check goal
    //  for (let x = 0; x < goals.length; x++){
    //
@@ -65,7 +66,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       for (let x = 0; x < 4; x++) {
          let offset = 300 * x;
          setTimeout(function() {
-            console.log('robots');
+            // console.log('robots');
             let robot = document.createElement('div');
             robot.className = 'robot';
             robot.style.backgroundImage = `url(https://robohash.org/${seed}/?set=set${set})`;
@@ -80,18 +81,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
                robot.style.left = robotPos + "px";
 
+
+
                if (Math.abs(robotPos) >= 1100) {
                   robotPos = -150;
                }
 
                requestAnimationFrame(moveRobot);
+               //  console.log('about to run collision');
+               checkCollision();
 
             };
             offset = offset + 100;
             moveRobot();
-            if (character && robot1) {
-               checkCollision();
-            }
+
 
          }, 3000);
 
@@ -103,7 +106,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
    function defineCharacter() {
-      console.log('characters');
+      // console.log('characters');
       let character = document.createElement('div');
       character.id = 'character';
       character.className = 'character';
@@ -182,15 +185,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
    }
 
    function checkKey(e) {
-      console.log('checking key');
+      // console.log('checking key');
       e = e || window.event;
       let direction = '';
       if (e.keyCode == '38') {
-         console.log('going up');
+         //  console.log('going up');
          move('up');
       } else if (e.keyCode == '40') {
          // down arrow
-         console.log('down');
+         //  console.log('down');
          move('down');
       } else if (e.keyCode == '37') {
          // left arrow
@@ -203,28 +206,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
    }
 
    function makeRobots() {
-      console.log('make r');
+      // console.log('make r');
    }
 
    function makeCharacters() {
-      console.log('make c');
+      // console.log('make c');
+   }
+
+   function collide() {
+      console.log('oh no!');
+      character.style.bottom = '0px';
+      character.style.left = '500px';
+      characterPosVert = 0;
+      characterPosHoriz = 500;
+      lives--;
+      console.log('lives left:', lives);
    }
 
    function checkCollision() {
-      robot1 = document.getElementById('robot1');
-      charLeft = parseInt((character.style.left).match(/[0-9]+/));
-      charRight = (parseInt((character.style.left).match(/[0-9]+/)) + character.offsetWidth);
-      charTop = parseInt((character.style.bottom).match(/[0-9]+/));
-      charBottom = (parseInt((character.style.bottom).match(/[0-9]+/)) + character.offsetHeight);;
-      robotLeft = parseInt((robot1.style.left).match(/[0-9]+/));
-      robotRight = (parseInt((robot1.style.left).match(/[0-9]+/)) + robot1.offsetWidth);
-      robotBottom = parseInt((robot1.style.bottom).match(/[0-9]+/));
-      robotTop = (parseInt((robot1.style.bottom).match(/[0-9]+/)) + robot1.offsetHeight);
+      // console.log('collision test running');
+
+      for (let y = 0; y < 5; y++) {
+
+         let robotName = 'robot' + (y);
+         robot1 = document.getElementById(`${robotName}`);
+         //  myRobot = document.getElementById(`${robotName}`);
+
+         if (robot1) {
+            charLeft = parseInt((character.style.left).match(/[0-9]+/));
+            charRight = (parseInt((character.style.left).match(/[0-9]+/)) + character.offsetWidth);
+            charTop = parseInt((character.style.bottom).match(/[0-9]+/));
+            charBottom = (parseInt((character.style.bottom).match(/[0-9]+/)) + character.offsetHeight);;
+            robotLeft = parseInt((robot1.style.left).match(/[0-9]+/));
+            robotRight = (parseInt((robot1.style.left).match(/[0-9]+/)) + robot1.offsetWidth);
+            robotBottom = parseInt((robot1.style.bottom).match(/[0-9]+/));
+            robotTop = (parseInt((robot1.style.bottom).match(/[0-9]+/)) + robot1.offsetHeight);
+            if ((((charBottom < robotTop) && (charBottom > robotBottom)) || ((charTop > robotBottom) && (charTop < robotTop))) && ((((charLeft > robotLeft) && (charLeft < robotRight)) || ((charRight > robotLeft) && (charRight < robotRight))))) {
+               console.log('collided!');
+               collide();
+
+            }
+         }
 
 
-
+         // ((charLeft > robotLeft) && (charLeft < robotRight))
+      }
    }
-
    // function setup game
 
    // define robot templates ()
@@ -391,6 +418,90 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 // every time robot moves
 // check if robot left, right, top, bottom coordinates match character
+// lives --
+// if lives = 0
+// alert modal - out of lives!
+// start button - direct back to index/reload page
+// move character back to beginning
+
+// every time character moves
+// check if character left, right, top, bottom coordinates match goal
+// leave a character or star etc in goal
+// update goal array
+// if goal array = full
+// alert modal - you win!
+// start button - direct back to index/reload page
+// move character back to beginning
+//back to beginning
+//caracter back to beginning
+//back to beginning
+// array of goals
+// goal 1
+// goal 2
+// goal 3
+// goal 4
+// goal 5
+// up, down, left, right
+
+// on DOM load
+// function run modal X
+// display modal X
+// listen for submit click X
+// store seed input value in seed variable X
+// hide modal X
+
+
+// setup game ()
+// run modal()
+
+// function setup game
+
+// define robot templates ()
+// make robots ()
+// create character template ()
+// make robots ()
+
+//define robot templates:
+// GET request(s) to robots/seed - 1 for character, 2 for monster types
+// define robots objects
+// define character object
+
+// make robots
+// generate 1robot() every x seconds, 5 times -- row 1 & 3
+// generate 1robot() type 2 every x seconds, 5 times - row 2
+// generate character();
+
+
+//generate 1 robot:
+// set properties
+// set speed & movement, start over
+
+//generate character:
+// set properties
+// set movement to equal key values
+
+//listen for key values after game setup
+
+// every time robot moves
+// check if robot left, right, top, bottom coordinates match character
+// lives --
+// if lives = 0
+// alert modal - out of lives!
+// start button - direct back to index/reload page
+// move character back to beginning
+
+// every time character moves
+// check if character left, right, top, bottom coordinates match goal
+// leave a character or star etc in goal
+// update goal array
+// if goal array = full
+// alert modal - you win!
+// start button - direct back to index/reload page
+// move character back to beginning
+//back to beginning
+//caracter back to beginning
+//back to beginning
+// right, top, bottom coordinates match character
 // lives --
 // if lives = 0
 // alert modal - out of lives!
