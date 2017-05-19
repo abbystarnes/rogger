86362ea1e5e35262cc6b3d93dd68cae7fd5c29f6
Abby Starnes <abbymstarnes@gmail.com>
2017-05-18 23:29:11 -0500

    multiple levels working

--- mainTWO.js
+++ mainTWO.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
    let modalText = document.getElementById('modalText');
    let modal = document.getElementById('modal');
    let seed = 'example';
+   let engineObj = null;
    // LEVEL GENERATION
    let level = 0;
    let botOptions = [1, 2, 3];
@@ -22,36 +23,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
    let l = -25;
    let r = +25;
 
+   let outcome = 'default';
 
    // MOVEMENT && SCORING
-   let playerPosHoriz = 190;
-   let playerPosVert = 0;
    let goalScore = [1, 1, 0, 1, 1];
    let goals = document.getElementsByClassName('goal');
    let lives = 3;
-  //  for (let z = 0; z)
-  //  let life = document.createElement('div');
-  //  life.classList.add('life');
-  //  container.append('life');
+
 
    // CREATE PLAYER & BOTS
+   let playerPosHoriz = 190;
+   let playerPosVert = 0;
    const player = document.getElementById('player');
    player.style.left = playerPosHoriz + 'px';
 
    // IF PLAYER TOUCHES A BOT, RESET POSITION & DECREMENT LIVES
-   function collide() {
 
-      console.log('oh no!');
-      playerPosVert = 0;
-      playerPosHoriz = 190;
-      player.style.bottom = playerPosVert + 'px';
-      player.style.left = playerPosHoriz + 'px';
-      lives--;
-      if (lives === 0) {
-         console.log('you lose');
-         setModal('loss');
-      }
-   }
 
    /// ARROW KEY INPUT TRIGGERS MOVEMENT FUNCTIONS
    function checkKey(e) {
@@ -60,22 +47,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
       let direction = '';
       if (e.keyCode == '38') {
         // right arrow
-         move('up');
+         mover('up');
       } else if (e.keyCode == '40') {
          // down arrow
-         move('down');
+         mover('down');
       } else if (e.keyCode == '37') {
          // left arrow
-         move('left');
+         mover('left');
       } else if (e.keyCode == '39') {
          // right arrow
-         move('right');
+         mover('right');
       }
     }
    }
 
    // MOVE PLAYER IN DIRECTION
-   function move(direction) {
+   function mover(direction) {
       if (direction === 'up') {
          if (playerPosVert < 600) {
             playerPosVert = playerPosVert + u;
@@ -130,53 +117,115 @@ document.addEventListener("DOMContentLoaded", function(event) {
          }
          if ((goalScore[0] === 1) && (goalScore[1] === 1) && (goalScore[2] === 1) && (goalScore[3] === 1) && (goalScore[4] === 1)) {
             console.log('you win it all!');
-            setModal('win');
+            outcome = 'win';
+            setModal();
          }
 
       }
    }
 
+  //  class Player {
+  //    constructor(playerPosHoriz, playerPosVert){
+  //      this.playerPosHoriz = playerPosHoriz;
+  //      this.playerPosVert = playerPosVert;
+  //    }
+  //    let playerPosHoriz = 190;
+  //    let playerPosVert = 0;
+  //  }
 
-   // CHECK FOR COLLISION
-   function checkCollision(botPosHoriz, botPosVert, bot) {
-      if (playerPosHoriz < (botPosHoriz + 50) && (playerPosHoriz + 50) > botPosHoriz && playerPosVert < (botPosVert + 50) && (playerPosVert + 50) > botPosVert) {
+  class Engine {
+    constructor(){
+      this.botObjs = [];
+      this.requestId = null;
+      this.MoveRobots();
+    }
+    InitRobots(botObjs){
+      this.botObjs = botObjs;
+    }
 
-        //
-        let ghost1 = document.createElement('div');
-        ghost1.style.position = 'absolute';
-        ghost1.style.left = botPosHoriz + 'px';
-        ghost1.style.width = 50 + 'px';
-        ghost1.style.bottom = botPosVert + 'px';
-        ghost1.style.height = 50 + 'px';
-        ghost1.style.backgroundColor = "rgba(63,91,63,.4)";
-        ghost1.style.display = 'block';
-        container.append(ghost1);
-
-        let ghost = document.createElement('div');
-        ghost.style.position = 'absolute';
-        ghost.style.left = playerPosHoriz + "px";
-        ghost.style.width = 50 + "px";
-        ghost.style.bottom = playerPosVert + 'px';
-        ghost.style.height = 50 + "px";
-        ghost.style.backgroundColor = "rgba(63,91,124,.4)";
-        ghost.style.display = 'block';
-        container.append(ghost);
-        bot.classList.add('zapped');
-        player.classList.add('zapped');
-        console.log(bot,'hit bot');
-        collide();
+    MoveRobots(){
+      for (let x = 0; x < this.botObjs.length; x++){
+        this.botObjs[x].move();
       }
+      this.requestId = window.requestAnimationFrame(this.MoveRobots.bind(this));
+    }
+
+    RemoveRobots(){
+      for (let x = 0; x < this.botObjs.length; x++){
+        this.botObjs[x].remove();
+      }
+      window.cancelAnimationFrame(this.requestId);
+    }
+  }
+
+   // create robot class
+   class Robot {
+     constructor(vertPosition, horizPosition, velocity, offset, set) {
+       this.verticalPosition = vertPosition;
+       this.horizontalPosition = horizPosition;
+       this.velocity = velocity;
+       this.offset = offset;
+       this.set = set;
+       this.level = level;
+       this.bot = this.create();
+       this.requestId = this.move();
+     }
+     remove(){
+       this.bot.parentNode.removeChild(this.bot);
+     }
+     create(){
+       let bot = document.createElement('div');
+       bot.className = 'bot';
+       bot.style.bottom = this.verticalPosition + 'px';
+       bot.style.backgroundImage = `url(http://galvanize-cors-proxy.herokuapp.com/https://robohash.org/${seed}?set=set${this.set})`;
+       container.append(bot);
+       bot.style.left = this.horizontalPosition + 'px';
+       return bot;
+     }
+     move() {
+          this.horizontalPosition = this.horizontalPosition + this.velocity;
+          this.bot.style.left = this.horizontalPosition + 'px';
+          if (Math.abs(this.horizontalPosition) >= 430) {
+             this.horizontalPosition = -50;
+          }
+          if ((this.horizontalPosition) < -50) {
+             this.horizontalPosition = 430;
+          }
+          this.checkCollision(this.horizontalPosition, this.verticalPosition);
+       }
+     checkCollision() {
+       if (playerPosHoriz < (this.horizontalPosition + 50) && (playerPosHoriz + 50) > this.horizontalPosition && playerPosVert < (this.verticalPosition + 50) && (playerPosVert + 50) > this.verticalPosition) {
+    
+         this.collide();
+       }
+     }
+     collide(){
+          console.log('oh no!');
+          playerPosVert = 0;
+          playerPosHoriz = 190;
+          player.style.bottom = playerPosVert + 'px';
+          player.style.left = playerPosHoriz + 'px';
+          lives--;
+          if (lives === 0) {
+             console.log('you lose');
+             outcome = 'loss';
+             setModal();
+          }
+     }
    }
+
 
    // CREATE GAME ON SCREEN
    function createGame() {
-
+     outcome = 'default';
+     engineObj = new Engine();
       // CREATE PLAYER
       let playerImage = `url(http://galvanize-cors-proxy.herokuapp.com/https://robohash.org/${seed}?set=set3)`;
       player.style.backgroundImage = playerImage;
 
       // CREATE BOTS
       let numRows = levels[level].length;
+      let botObjs = [];
       for (let x = 0; x < numRows; x++) {
          let row = levels[level][x];
          let botsPerRow = row.botPicks.length;
@@ -184,71 +233,48 @@ document.addEventListener("DOMContentLoaded", function(event) {
          for (let y = 0; y < botsPerRow; y++) {
            let botPosVert = 0;
            let botPosHoriz = 0;
-            let bot = document.createElement('div');
-            let set = levels[level][x].botPicks[y];
-            bot.className = 'bot';
+           let set = levels[level][x].botPicks[y];
             if (x < 5) {
                botPosVert = ((x + 1) * 50);
-               bot.style.bottom = botPosVert + 'px';
             } else {
                botPosVert = ((x + 2) * 50);
-               bot.style.bottom = botPosVert + 'px';
             }
-            bot.style.backgroundImage = `url(http://galvanize-cors-proxy.herokuapp.com/https://robohash.org/${seed}?set=set${set})`;
-            container.append(bot);
+
             botPosHoriz = (((offset) + 50) * y);
-            bot.style.left = botPosHoriz + 'px';
 
-            function moveBot() {
-               botPosHoriz = botPosHoriz + row.velocity;
-               bot.style.left = botPosHoriz + 'px';
-               if (Math.abs(botPosHoriz) >= 430) {
-                  botPosHoriz = -50;
-               }
-
-               if ((botPosHoriz) < -50) {
-                  botPosHoriz = 430;
-
-               }
-               requestAnimationFrame(moveBot);
-               checkCollision(botPosHoriz, botPosVert, bot);
-            }
-              moveBot();
+            botObjs.push(new Robot(botPosVert, botPosHoriz, row.velocity, offset, set));
          }
       }
+      engineObj.InitRobots(botObjs);
    }
+
    document.onkeydown = checkKey;
 
-
-
+   submit.addEventListener('click', function(event) {
+     event.preventDefault();
+      if (outcome === 'win') {
+         level++;
+         console.log(level, 'level');
+         engineObj.RemoveRobots();
+         createGame();
+      } else if (outcome === 'loss') {
+         location.reload();
+      } else {
+         if (getSeed.value !== ''){
+           seed = getSeed.value;
+         }
+         console.log(seed, 'seed');
+         if (lives > 0){
+           createGame();
+         }
+      }
+      modal.classList.add('hidden');
+   });
 
    // SET UP MODAL
-   function setModal(outcome) {
+   function setModal() {
       modal.classList.remove('hidden');
-      submit.addEventListener('click', function(event) {
-         if (outcome === 'win') {
-            level++;
-            console.log(level, 'level');
-            let bots = document.getElementsByClassName('bot');
-            let bots2 = Array.prototype.slice.call( bots );
-            for ( let x = 0; x < bots2.length; x++){
-              bots2[x].parentNode.removeChild(bots2[x]);
-            }
-            createGame();
-         } else if (outcome === 'loss') {
-            location.reload();
-         } else {
-            event.preventDefault();
-            if (getSeed.value !== ''){
-              seed = getSeed.value;
-            }
-            console.log(seed, 'seed');
-            if (lives > 0){
-              createGame();
-            }
-         }
-         modal.classList.add('hidden');
-      });
+
 
       if (outcome === 'win') {
          modalText.innerHTML = 'Continue to next level!';
@@ -262,7 +288,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
          getSeedLabel.classList.add('hidden');
       }
    }
-
    setModal();
 
 });
